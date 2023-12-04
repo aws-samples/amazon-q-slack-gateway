56492e646e7f83cc52f08fd5b9bf479a622a5a31
rstrahan <strahanr@amazon.com>
2023-12-04 21:43:48 +0000

    fix lint/prettier configuration to a_ include bin directory, b) exclude cdk.out and build directories, c) to include javascript files. Run linter and apply fixes on source

--- bin/my-amazon-q-slack-bot.ts
+++ bin/my-amazon-q-slack-bot.ts
@@ -5,9 +5,9 @@ import { MyAmazonQSlackBotStack } from '../lib/my-amazon-q-slack-bot-stack';
 import { readFileSync } from 'fs';
 
 export interface StackEnvironment {
-  StackName: string
-  AmazonQAppId: string
-  AmazonQUserId: string
+  StackName: string;
+  AmazonQAppId: string;
+  AmazonQUserId: string;
   AmazonQRegion: string;
   AmazonQEndpoint?: string;
   ContextDaysToLive: string;
@@ -15,15 +15,32 @@ export interface StackEnvironment {
 
 const app = new cdk.App();
 const inputEnvFile = app.node.tryGetContext('environment');
-if (inputEnvFile === undefined) { throw new Error("An input environment file is required"); }
+if (inputEnvFile === undefined) {
+  throw new Error('An input environment file is required');
+}
 
 const environment = JSON.parse(readFileSync(inputEnvFile).toString()) as StackEnvironment;
-if (environment.StackName === undefined) { throw new Error("StackName is required"); }
-if (environment.AmazonQAppId === undefined) { throw new Error("AmazonQAppId is required"); }
-if (environment.AmazonQRegion === undefined) { throw new Error("AmazonQRegion is required"); }
-if (environment.AmazonQUserId === undefined) { throw new Error("AmazonQUserId is required"); }
-if (environment.ContextDaysToLive === undefined) { throw new Error("ContextDaysToLive is required"); }
+if (environment.StackName === undefined) {
+  throw new Error('StackName is required');
+}
+if (environment.AmazonQAppId === undefined) {
+  throw new Error('AmazonQAppId is required');
+}
+if (environment.AmazonQRegion === undefined) {
+  throw new Error('AmazonQRegion is required');
+}
+if (environment.AmazonQUserId === undefined) {
+  throw new Error('AmazonQUserId is required');
+}
+if (environment.ContextDaysToLive === undefined) {
+  throw new Error('ContextDaysToLive is required');
+}
 
-new MyAmazonQSlackBotStack(app, 'AmazonQSlackGatewayStack', {
-  stackName: environment.StackName,
-}, environment);
+new MyAmazonQSlackBotStack(
+  app,
+  'AmazonQSlackGatewayStack',
+  {
+    stackName: environment.StackName
+  },
+  environment
+);
